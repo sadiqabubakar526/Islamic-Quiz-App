591857b32ac6e322ee60ee2b1f2597a2c21c5170
Suraj Abubakar <surajabubakar443@gmail.com>
2021-06-27 00:14:41 +0100

    Add files via upload

---
+++ src/Questions.js
@@ -0,0 +1,53 @@
+export  const questions = [
+    {
+        question:'What do we call the Angels who write down what we do ?',
+        options:['Israfil','Kiraman Katibin','Mikail', 'Izrail'],
+        answer:'Kiraman Katibin'
+    },
+    {
+        question:'The Muslims started fasting in the month of Ramadan,in what year?',
+        options:['2 years after hijra','On Prophet burial','From beginning of the world','3rd year after hijra'],
+        answer:'3rd year after hijra'
+    },
+     {
+       question:'THE SMOKE is a surah in the QURAN termed ____ in arabic',
+       options:['Surah-Rum','Surah-Qalam','Surah-Layl','Surah-Dukhan'],
+       answer:'Surah-Dukhan'
+     },
+    {
+        question:'The Holy QURAN was revealed in the period of _____ ?',
+        options:['20years','23years','9months','63years'],
+        answer:'23years'
+    },
+    {
+        question:'The first kid to accept Islam is ____?',
+        options:['Uthman ibn Affan','Prophet Shuaib','Ali ibn Abi Talib','Abubakar ibn Quhaffah'],
+        answer:'Ali ibn Abi Talib'
+    },
+    {
+        question:' Pillars of Islam are also called ...…',
+        options:['Qadr ul Islam','Arkanal Islam','Yusuf Islam','Fi Amanillah'],
+        answer:'Arkanal Islam'
+    },
+    {
+        question:' A prophet is called .......... in Arabic.',
+        options:['Nabi','Rasul','Wahi', 'None of the above'],
+        answer:'Nabi'
+    },
+    {
+        question:'What does Islam mean ?',
+        options:['Islam means to obey Allah (S.W.T.) and follow His commands','Islam means to obey our parents','Islam means to obey our teachers and elders', 'None of the above'],
+        answer:'Islam means to obey Allah (s.w.t.) and follow His commands'
+    },
+    {
+        question:'Islam has how many pillars ?',
+        options:['8 pillars','5 pillars','3 pillars','10 pillars'],
+        answer:'5 pillars'
+  
+    },
+    {
+        question:'Allah (s.w.t.) has ......…',
+        options:['A son','A partner','No partner','A daughter'],
+        answer:'No partner'
+    },
+]
