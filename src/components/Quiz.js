284523c9e1de03fe39eb1b520145b10f78a0e755
Suraj Abubakar <surajabubakar443@gmail.com>
2021-06-26 12:41:40 +0100

    Add files via upload

---
+++ src/components/Quiz.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+class Quiz extends Component {
+	componentDidMount(){
+		this.props.methods.getNewQuestion()
+	}
+	// state ={delay:0.2}
+	disable=(elem)=>{
+		const length = elem.children.length;
+		for(let i =0; i<length; i++){
+		elem.children[i].classList.add('already-answered')
+	}
+	}
+	getResult = (e)=>{
+		this.props.methods.getResult(e);
+		this.disable(e.target.parentElement)
+	}
+	render(){
+		const {question, options, count, total,
+			 correctness} =this.props.properties;
+		const {getNewQuestion} = this.props.methods;		 
+		const myOptions = document.querySelectorAll('.option');
+			myOptions.forEach(option=>{
+				if(!correctness)
+				option.className="option"k
+			})
+		return ( 
+	   <div className="quiz box">
+		<div className="question-number"> Question {count} of  {total} </div>
+		<div className="question-text">{question}</div>
+		<div className="option-container">
+		{
+			options.map((option, i)=>(
+			<div className="option" 
+			onClick={(e)=>this.getResult(e)}
+			style={{animationDelay:''}}
+			key ={i}>{option}</div>
+			))
+		}	
+		</div>
+		<div className="next-question-btn">
+		
+			<button className="btn" onClick ={()=>getNewQuestion()}>Next</button>
+			
+		</div>
+		<div className="answers-indicator">
+		<div id ='0'></div>
+		<div id ='1'></div>
+		<div id ='2'></div>
+		<div id ='3'></div>
+		<div id ='4'></div>
+		<div id ='5'></div><br>
+		<div id ='6'></div>
+  	<div id ='7'></div>
+		<div id ='8'></div>	
+		<div id ='9'></div>	
+		<div id ='10'></div>	
+		</div>
+	
+	</div>
+		 );
+	}
+}
+ 
+export default Quiz;
